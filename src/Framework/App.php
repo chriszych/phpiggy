a70d21e5c53ee56201166f398a509527725c9b3d
chriszych <krzysztof.zych.programista@gmail.com>
2025-08-04 22:53:18 +0200

    Render a 404 Page
    Render a 404 Page for supporting not defined routes

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -52,4 +52,9 @@ class App {
         $this->router->add('DELETE', $path, $controller);
         return $this;
     }
+
+    public function setErrorHandler(array $controller)
+    {
+        $this->router->setErrorHandler($controller);
+    }
 }
